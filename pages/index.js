177791f31ecb5d50847128e1bf274880c1ee640a
Jair <jairaceves56@gmail.com>
2023-04-02 09:27:05 -0600

    Importamos las categorias

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
+import { PrismaClient } from "@prisma/client"
 
-export default function Home() {
+export default function Home({categorias}) {
+
+  
+
   return (
     <>
         <h1 className='text-3xl text-center'>Hola mundo usando Next.js</h1>
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient()
+  const categorias = await prisma.categoria.findMany()
+
+  return {
+    props: {
+      categorias,
+    }
+  }
 }
