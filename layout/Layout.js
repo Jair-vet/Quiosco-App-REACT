dc4576f1f625eee1a3f7e994555e1773f35ed4cb
Jair <jairaceves56@gmail.com>
2023-04-14 09:02:30 -0600

    Agregamos el componente ModalProducto para mostar informacion en el modal

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,3 +1,4 @@
+import { ModalProducto } from "@/components/ModalProducto";
 import { Sidebar } from "@/components/Sidebar";
 import useQuiosco from "@/hooks/useQuiosco";
 import Head from "next/head";
@@ -44,7 +45,7 @@ export default function Layout({children, pagina}) {
                     isOpen={modal}
                     style={customStyles}
                 >
-                    <h1>Modal..</h1>
+                    <ModalProducto />
                 </Modal>
             )}
         </>
