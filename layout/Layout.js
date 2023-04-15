65105d92ce32b4cf0358f44b33a0289543f52882
Jair <jairaceves56@gmail.com>
2023-04-15 12:15:00 -0600

    Agregamos el el Layout el Menu

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,4 +1,5 @@
 import { ModalProducto } from "@/components/ModalProducto";
+import { Pasos } from "@/components/Pasos";
 import { Sidebar } from "@/components/Sidebar";
 import useQuiosco from "@/hooks/useQuiosco";
 import Head from "next/head";
@@ -38,6 +39,7 @@ export default function Layout({children, pagina}) {
 
                 <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
                     <div className="p-10">
+                        <Pasos />
                         {children}
                     </div>
                 </main>
